25ef043ee066117ad5ff38d7633376f70dca7479
Arifac83 <60567004+Arifac83@users.noreply.github.com>
2020-10-10 15:05:55 +0600

    updated heroku

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const admin = require('firebase-admin');
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config()
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cfjij.mongodb.net/volunteerNetwork?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cfjij.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const port = 5000;
 const app = express()
